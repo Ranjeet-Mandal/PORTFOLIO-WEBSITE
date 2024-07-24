8f1d20b140f64c380aaeaaf72e74fe683eb80ff0
Ranjeet Mandal <ranjeetmandal000@gmail.com>
2024-07-24 18:53:17 +0530

    initial commit

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -4,37 +4,43 @@ import project3_img from '../assets/project_3.svg'
 import project4_img from '../assets/project_4.svg'
 import project5_img from '../assets/project_5.svg'
 import project6_img from '../assets/project_6.svg'
+import project10_img from '../assets/project_10.jpg'
+import project9_img from '../assets/project_9.jpg'
+import project8_img from '../assets/project_8.jpg'
+import project11_img from '../assets/project_11.jpg'
+import project12_img from '../assets/project_12.jpg'
+import project13_img from '../assets/project_13.jpg'
 
 const mywork_data = [
     {
         w_no:1,
         w_name:"Web design",
-        w_img:project1_img
+        w_img:project10_img
     },
     {
         w_no:2,
         w_name:"Web design",
-        w_img:project2_img
+        w_img:project9_img
     },
     {
         w_no:3,
         w_name:"Web design",
-        w_img:project3_img
+        w_img:project13_img
     },
     {
         w_no:4,
         w_name:"Web design",
-        w_img:project4_img
+        w_img:project8_img
     },
     {
         w_no:5,
         w_name:"Web design",
-        w_img:project5_img
+        w_img:project11_img
     },
     {
         w_no:6,
         w_name:"Web design",
-        w_img:project6_img
+        w_img:project12_img
     },
 ]
  
